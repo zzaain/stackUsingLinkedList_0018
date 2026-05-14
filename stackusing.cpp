3532c08827a8c490c54c933df13f168275caf880
zzaain <sheriellaghzain@gmail.com>
2026-05-14 15:13:26 +0700

    Menambahkan case 4 untuk exit, default handling pilihan salah, dan menutup fungsi main

--- stackusing.cpp
+++ stackusing.cpp
@@ -114,3 +114,14 @@ int main()
                 cout << "Stack is empty.No top value." << endl;
             }
             break;
+            case 4:
+            cout << "Exiting program." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
+        }
+        cout << endl;
+    }
+    return 0;
+}
