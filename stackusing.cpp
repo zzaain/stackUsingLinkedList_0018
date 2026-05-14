8db80be7a615f37b52954a436ef8b64b590aada4
zzaain <sheriellaghzain@gmail.com>
2026-05-14 15:12:53 +0700

    Menambahkan penanganan case 3 untuk pengecekan kondisi dan pemicu fungsi peek

--- stackusing.cpp
+++ stackusing.cpp
@@ -104,3 +104,13 @@ int main()
                 cout << "Stack is empty. Cannot pop." << endl;
             }
             break;
+            case 3:
+            if (!stack.isEmpty())
+            {
+                stack.peek();
+            }
+            else
+            {
+                cout << "Stack is empty.No top value." << endl;
+            }
+            break;
